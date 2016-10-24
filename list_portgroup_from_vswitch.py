c2af4953b084650fe7980dc2574b40b365f72646
Luckylau <laujunbupt0203@163.com>
2016-10-24 21:32:53 +0800

    list portgroup

---
+++ list_portgroup_from_vswitch.py
@@ -0,0 +1,81 @@
+#
+# by lucky lau
+#
+# email :laujunbupt0913@163.com
+#
+#
+import sys
+from pyVim.connect import SmartConnect,Disconnect
+import atexit
+from pyVmomi import vim
+
+def getarg():
+    if len(sys.argv)>1:
+        host,user,password,vswitch=sys.argv[1:]
+    else:
+        host = raw_input("Host IP : ")
+        user = raw_input("Username: ")
+        password = raw_input("Password: ")
+        vswitchname= raw_input("Vswitch name: ")
+    return host,user,password,vswitchname
+
+def get_obj(content,vimtype):
+    obj=None
+    container = content.viewManager.CreateContainerView(
+        content.rootFolder, vimtype, True
+    )
+    for host in container.view:
+        obj=host
+    return obj
+
+def get_vswitch(host,name):
+    obj=None
+    if host:
+        for vswitch in host.config.network.vswitch:
+            if vswitch.name == name:
+                obj = vswitch
+                break
+    else:
+        print "Host not found"
+    return obj
+
+def list_portgroup(host,vswitch):
+    if vswitch.portgroup:
+        print "portgroup info:\n"
+        for pg in vswitch.portgroup:
+            for portgroup in host.config.network.portgroup:
+                if portgroup.key==pg:
+                    print "name :"+portgroup.spec.name + " vlanId :" + str(portgroup.spec.vlanId)
+    else:
+        print " Portgroup not find ..."
+
+
+def main():
+    host,user,password,vswitchname=getarg()
+    #host="10.0.36.17"
+    #user="root"
+    #password="1111111"
+    #vswitchname="vSwitch1"
+    default_port="443"
+
+    serviceInstance = SmartConnect(host=host,
+                                   user=user,
+                                   pwd=password,
+                                   port=default_port)
+
+    atexit.register(Disconnect, serviceInstance)
+
+    print "Search Virtual Switch by name ..."
+
+    vswitch=None
+    host=None
+    content = serviceInstance.RetrieveContent()
+    host= get_obj(content,[vim.HostSystem])
+    vswitch=get_vswitch(host,vswitchname)
+    if vswitch:
+        print "Find Virtual Switch , Search portgroup ..."
+        list_portgroup(host,vswitch)
+    else:
+        print "Virtual Switch not find ..."
+if __name__ == '__main__':
+    main()
